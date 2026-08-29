7265418264f004b3ca90f37890007677d04f62a2
Sepehr Nasirzadeh <info.nasirzadeh@gmail.com>
2026-08-29 23:10:25 +0330

    Change monitor.sh script

--- monitoring/monitor.sh
+++ monitoring/monitor.sh
@@ -6,24 +6,27 @@ set -Eeuo pipefail
 LOG_DIR="/home/sepehr/github/devops-scenario/logs"
 MONITOR_DIR="/home/sepehr/github/devops-scenario/monitoring"
 DATE=$(date '+%Y%m%d')
-
-ACCESS_LOG="$LOG_DIR/access.log-$DATE"
-ERROR_LOG="$LOG_DIR/error.log-$DATE"
+REPORT="$MONITOR_DIR/nginx.log.$DATE"
 
 logme() {
-
-    awk '{print $1}' "$ACCESS_LOG" |
-        sort | uniq -c | sort -nr | head -3 > "$MONITOR_DIR/access.log"
-
-    grep -E '(^|[^0-9])404([^0-9]|$)' "$ERROR_LOG" > "$MONITOR_DIR/error.log" || true
+    {
+        echo "Top 3 IP addresses by request count:"
+        grep -h "$(date '+%d/%b/%Y')" "$LOG_DIR"/access.log* |
+            awk '{print $1}' |
+            sort | uniq -c | sort -nr | head -3
+        echo "----------------------------------------"
+        echo "HTTP 404 Errors:"
+        grep -h "$(date '+%d/%b/%Y')" "$LOG_DIR"/access.log* |
+            awk '$9 == 404'
+    } > "$REPORT"
 }
+
 email() {
     mail \
-        -s "Nginx Daily Monitoring Report - $DATE" \
+        -s "Nginx Daily Report - $DATE" \
         "debian" \
-        < <(cat "$MONITOR_DIR/access.log.$DATE" "$MONITOR_DIR/error.log.$DATE")
+        < "$REPORT"
 }
 
 logme
 email
-
